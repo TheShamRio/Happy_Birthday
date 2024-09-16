6ac57b90631e8810ddfa4703a51f9dd907439247
TheShamRio <xsham@inbox.ru>
2024-09-16 16:08:34 +0300

    Text Size and LineHeught

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 
@@ -34,8 +35,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, modifier: Modifier = Modifier) {
-    Text(
-        text = message
+    Text (
+        text = message,
+        fontSize = 100.sp,
+        lineHeight = 116.sp,
     )
 }
 
